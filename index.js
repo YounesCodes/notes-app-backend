6393643ae1401834fc837fe22f981ae539bb2909
YounesCodes <younesbouatmani1@gmail.com>
2025-11-06 19:57:54 +0100

    added delete and put functionality

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
     console.log('Body:  ', request.body)
-    console.log('---')
+    console.log('----------')
     next()
 }
 
@@ -30,21 +30,18 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id).then(note => {
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
     
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
-    return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -64,17 +61,50 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
+app.put('/api/notes/:id', (request, response, next) => {
+    const {content, important} = request.body
+
+    Note.findById(request.params.id)
+     .then(note => {
+        if (!note) {
+            return response.status(404).end()
+        }
+
+        note.content = content
+        note.important = important
+
+        return note.save().then((updatedNote) => {
+            response.json(updatedNote)
+        })
+     })
+     .catch(error => next(error))
 })
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+     .then(result => {
+        response.status(204).end()
+     })
+     .catch(error => next(error))
+})
+
+
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({error: 'unknown endpoint'})
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError'){
+        return response.status(400).send({error:'malformatted id'})
+    }
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
