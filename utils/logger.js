364bd0a4aa27a80897f68c82569b4c1667a73777
YounesCodes <younesbouatmani1@gmail.com>
2025-11-09 00:18:56 +0100

    implemented some tests + async await routing

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,13 @@
 const info = (...params) => {
-  console.log(...params)
+  if (process.env.NODE_ENV !== 'test'){ // dont clog in test mode pls and ty
+    console.log(...params)
+  }
 }
 
 const error = (...params) => {
-  console.error(...params)
+  if (process.env.NODE_ENV !== 'test'){
+    console.error(...params)
+  }
 }
 
 module.exports = { info, error }
